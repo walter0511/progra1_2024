24b45f8cd4a958a6a9ee7113f98b6c727f6e7cfa
walter0511 <walterbenavidesernesto@gmail.com>
2024-08-19 23:13:10 -0600

    suma de dos numeros

--- progra1_2024/progra1_2024/Program.cs
+++ progra1_2024/progra1_2024/Program.cs
@@ -10,8 +10,18 @@ namespace progra1_2024
     {
         static void Main(string[] args)
         {
-            // este es un comentario
-            // segundo comentario
+            Console.Write("Num1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int respuesta = num1 + num2;
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+            //Pausa.
+            Console.ReadLine();
+
         }
     }
 }
