435abfd635a689b9236d8d339105c3a8fd89fa91
walter0511 <walterernestobenavides@gmail.com>
2024-08-19 22:05:28 -0600

    primer comentario

---
+++ progra1_2024/progra1_2024/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progra1_2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // este es un comentario
+        }
+    }
+}
