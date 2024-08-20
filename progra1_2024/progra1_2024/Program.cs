83580f33574da9614cc458c927dc123edf123422
walter0511 <walterbenavidesernesto@gmail.com>
2024-08-19 23:57:07 -0600

    promedio de notas

--- progra1_2024/progra1_2024/Program.cs
+++ progra1_2024/progra1_2024/Program.cs
@@ -10,14 +10,24 @@ namespace progra1_2024
     {
         static void Main(string[] args)
         {
-            Console.Write("Num1: ");
-            int num1 = int.Parse(Console.ReadLine());
+       
+            double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+            Console.WriteLine("La nota de C1 es: {0}", c1);
 
-            Console.Write("Num 2: ");
-            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Lab1: ");
+            lab1 = double.Parse(Console.ReadLine()); //8
 
-            int respuesta = num1 + num2;
-            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+            Console.Write("Lab2: ");
+            lab2 = double.Parse(Console.ReadLine()); //9
+
+            Console.Write("Parcial 1: ");
+            parcial1 = double.Parse(Console.ReadLine()); //7
+
+            double c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+            Console.WriteLine("La nota de C2 es: {0}", c2);
+
+            //TAREA: Obtener la nota final por computo y Nota Final de la matera de Programacion I.
+
 
             //Pausa.
             Console.ReadLine();
