0527613dadbdcb8e7062759f8c1fc2f47819efba
walter0511 <walterbenavidesernesto@gmail.com>
2024-08-20 23:33:39 -0600

    creacion de matriz

--- progra1_2024/progra1_2024/Program.cs
+++ progra1_2024/progra1_2024/Program.cs
@@ -10,43 +10,30 @@ namespace progra1_2024
     {
         static void Main(string[] args)
         {
-            string continuar = "s";
-            while (continuar == "s")
-            {
-                Console.Write("Edad: ");
-                int edad = int.Parse(Console.ReadLine());
+            double[,] matriz = new double[,] {
+                {0.01, 487.6, 0, 0 },
+                {487.61, 642.85, 10, 17.48},
+                {642.86, 915.81, 10, 32.70},
+                {915.82, 2058.67, 20,60.00},
+                {2058.68, 9999999, 30, 288.57},
+            };
+            Console.Write("sueldo");
+            double sueldo = double.Parse(Console.ReadLine());
+            double afp = sueldo * 6.25 / 100;
+            sueldo -= afp;
 
-                if (edad < 0)
-                {
-                    Console.WriteLine("Edad incorrecta.");
-                }
-                else if (edad <= 2)
-                {
-                    Console.WriteLine("Eres un bebe");
-                }
-                else if (edad < 12)
-                {
-                    Console.WriteLine("Eres un niño");
-                }
-                else if (edad < 18)
-                {
-                    Console.WriteLine("Eres un adolescente.");
-                }
-                else if (edad <= 65)
-                {
-                    Console.WriteLine("Bienvenido al mundo de las reposabilidades.");
-                }
-                else if (edad <= 80)
-                {
-                    Console.WriteLine("Eres un adulto mayor");
-                }
-                else
-                {
-                    Console.WriteLine("Larga vida");
-                }
-                Console.Write("Desea continuar presione s, sino cualquier tecla.");
-                continuar = Console.ReadLine();
+            double isr = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (sueldo >= matriz[i, 0] && sueldo <= matriz[i, 1])
+                    isr = (sueldo - (matriz[i, 0] - 0.01)) * matriz[i, 2] / 100 + matriz[i, 3];
             }
+
+            sueldo -= isr;
+            Console.WriteLine("Sueldo: {0}, AFP: {1}, ISR: {2}", sueldo, afp, isr);
+
+            Console.ReadLine();
+
         }
     }
 }
