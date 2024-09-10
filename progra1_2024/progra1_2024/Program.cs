4b5b5be2ab918c5c1b43b1fb4b5e56d422e2c436
walter0511 <walterbenavidesernesto@gmail.com>
2024-09-09 21:33:11 -0600

    uso de funciones con parametros

--- progra1_2024/progra1_2024/Program.cs
+++ progra1_2024/progra1_2024/Program.cs
@@ -13,12 +13,27 @@ namespace progra1_2024
             //Uso de funciones...
             Console.WriteLine("La suma de 10+5={0}", suma());
 
+            Console.Write("Num 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("La suma de {0}+{1}={2}", num1, num2, suma(num1, num2));
+
             Console.ReadLine();
         }
         static int suma()
         {
             int respuesta = 10 + 5;
             return respuesta;
+        }
+        static int suma(int a, int b)
+        {
+            int respuesta = a + b;
+            return respuesta;
+
+
         }
 
 
