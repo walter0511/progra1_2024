55e8a5bdb1476fe61649349de626df758e89b6e9
walter0511 <walterbenavidesernesto@gmail.com>
2024-09-09 21:22:53 -0600

    ejemplo de fuunciones

--- progra1_2024/progra1_2024/Program.cs
+++ progra1_2024/progra1_2024/Program.cs
@@ -10,36 +10,20 @@ namespace progra1_2024
     {
         static void Main(string[] args)
         {
-            {
-            //determinar si cada numero de la serie es primo.
+            //Uso de funciones...
+            Console.WriteLine("La suma de 10+5={0}", suma());
 
-              int[]  serie =  new int[] { 5, 9, 4, 6, 3, 2, 7, 11, 15, 17, 21 };
-               foreach (int num in serie)
-               {
-                 Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "Par" : "Impar");
-                int i = 1,
-                    n = 0;
-                while (i <= num && n <= 2)
-                {
-                    if (num % i == 0)
-                    {
-                        n++;//n=n+1
-                    }
-                    i++; //i=i+1
-                }
-                Console.WriteLine("EL numero {0} {1}", num, n <= 2 ? "es primo" : "NO es primo");
-
-                    Console.ReadLine();
-                }
-
-
-
-
-                
-
-
-
-            }
+            Console.ReadLine();
         }
+        static int suma()
+        {
+            int respuesta = 10 + 5;
+            return respuesta;
+        }
+
+
+
+
     }
+   
 }
