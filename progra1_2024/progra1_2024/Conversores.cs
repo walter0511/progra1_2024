7307ff2f7e9dfa4bb4854655ea0bc77e6d24fc9b
walter0511 <walterbenavidesernesto@gmail.com>
2024-09-17 00:50:23 -0600

    resolucion de conversores usando programacion orientada

---
+++ progra1_2024/progra1_2024/Conversores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progra1_2024
+{
+    class Conversores
+    {
+
+        double[][] valores = new double[][] {
+            new double[] {0,1,7.73,24.76,36.80, 517.04,8.75,0.9},//monedas
+            new double[] {0,1,100,39.37,3.28084,1.196308,1.09361,0.001},//longitud
+            new double[] {0,1,453.592,16,0.453592, 0.000446429}, //masa
+            new double[] {0,1,86400, 1440, 24, 0.142857, 0.032876643423, 0.0027397232876831892345}//tiempo
+            };
+        public double convertir(int de, int a, double cantidad, int opcion)
+        {
+
+            return valores[opcion][a] / valores[opcion][de] * cantidad;
+        }
+    }
+}
+
--- progra1_2024/progra1_2024/Program.cs
+++ progra1_2024/progra1_2024/Program.cs
@@ -10,101 +10,41 @@ namespace progra1_2024
     {
         static void Main(string[] args)
         {
-            //uso de funciones
+            Conversores objConversor = new Conversores();
+            String continuar = "s";
+            while (continuar == "s")
             {
-                while (true)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Menú Principal:");
-                    Console.WriteLine("1. Calcular Impuesto de Actividades Económicas");
-                    Console.WriteLine("2. Conversor de Unidades de Área");
-                    Console.WriteLine("3. Salir");
-                    Console.Write("Seleccione una opción: ");
-                    int opcion = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("::.. MENU ..::");
+                Console.WriteLine("1. Monedas.");
+                Console.WriteLine("2. Longitud.");
+                Console.WriteLine("3. Masa.");
+                Console.WriteLine("4. Tiempo.");
+                Console.WriteLine("0. Salir");
+                Console.Write("Opcion: ");
+                int opcion = int.Parse(Console.ReadLine());
 
-                    switch (opcion)
+                if (opcion == 0)
+                {
+                    continuar = "n";
+                }
+                else
+                {
+                    for (int i = 1 < objConversor.etiquetas[opcion].lenght; i++)
                     {
-                        case 1:
-                            Calcularimpuesto();
-                            break;
-                        case 2:
-                            Conversorarea();
-                            break;
-                        case 3:
-                            Console.WriteLine("Saliendo del programa...");
-                            return;
-                        default:
-                            Console.WriteLine("Opción no válida. Intente de nuevo.");
-                            break;
+
                     }
+                    Console.WriteLine("{0}. {1}", i, objConversor.etiquetas[opcion][i]);
+                    Console.Write("De: ");
+                    int de = int.Parse(Console.ReadLine());
 
-                    Console.WriteLine("\nPresione cualquier tecla para volver al menú principal...");
-                    Console.ReadKey();
+                    Console.Write("A: ");
+                    int a = int.Parse(Console.ReadLine());
+
+                    Console.Write("Cantidad: ");
+                    double cantidad = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("{0} \n", objConversor.convertir(de, a, cantidad, opcion));
                 }
             }
-
-            static void Calcularimpuesto()
-            {
-                Console.Clear();
-                Console.WriteLine("Impuesto De Actividades Económicas");
-                Console.Write("Ingrese el valor económico de la actividad: ");
-                decimal actividad = Convert.ToDecimal(Console.ReadLine());
-
-                decimal rango = 1000.01m;
-                decimal valorf = 1000m;
-                decimal adicional = 3m;
-                decimal precio = 3m;
-                decimal monto;
-
-                decimal diferencia = actividad - rango;
-                monto = (diferencia / valorf) * adicional + precio;
-
-                Console.WriteLine("El valor económico de la actividad económica es: $" + actividad);
-                Console.WriteLine("Monto a pagar es: $" + monto.ToString("F2"));
-            }
-
-            static void Conversorarea()
-            {
-                var conversion = new Dictionary<string, double>
-        {
-            { "Pie Cuadrado", 0.0929 },
-            { "Vara Cuadrada",  0.698896  },
-            { "Yardas cuadrada", 0.836127 },
-            { "Metro cuadrado", 1 },
-            { "Tareas", 1000 },
-            { "Manzana", 7050 },
-            { "Hectarea", 10000 }
-        };
-
-                Console.Clear();
-                var unidades = new List<string>(conversion.Keys);
-
-                Console.WriteLine("Seleccione el dato de entrada:");
-                mostraropciones(unidades);
-
-                int Entrada = Convert.ToInt32(Console.ReadLine()) - 1;
-
-                Console.WriteLine("Conversor de Unidades de Área: ");
-                Console.Write("Ingrese la cantidad a convertir: ");
-                double cantidad = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Seleccione el dato de salida:");
-                mostraropciones(unidades);
-
-                int Salida = Convert.ToInt32(Console.ReadLine()) - 1;
-
-                string entrada = unidades[Entrada];
-                string salida = unidades[Salida];
-                double resultado = cantidad * conversion[entrada] / conversion[salida];
-
-                Console.WriteLine($"La cantidad de {cantidad} {entrada} es equivalente a {resultado} {salida}.");
-            }
-
-            static void mostraropciones(List<string> opciones)
-            {
-                for (int i = 0; i < opciones.Count; i++)
-                {
-                    Console.WriteLine($"{i + 1}. {opciones[i]}");
-                }
-            }   }
+        }
 }   }
