61b945db73b6a110157e141f8425ce50d197fcf6
walter0511 <walterbenavidesernesto@gmail.com>
2024-09-23 22:51:11 -0600

    Creacion del diseño del formulario de alumnos

---
+++ formulario1/formulario1/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace formulario1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LblCodigoAlumno = new System.Windows.Forms.Label();
+            this.LblNombreDelAlumno = new System.Windows.Forms.Label();
+            this.LblDireccionDelAlumno = new System.Windows.Forms.Label();
+            this.LblTelefonoDelAlumno = new System.Windows.Forms.Label();
+            this.LblDuiDelAlumno = new System.Windows.Forms.Label();
+            this.tbxCodigoAlumnos = new System.Windows.Forms.TextBox();
+            this.tbxNombreAlumnos = new System.Windows.Forms.TextBox();
+            this.tbxDireccionAlumnos = new System.Windows.Forms.TextBox();
+            this.tbxTelefonoAlumnos = new System.Windows.Forms.TextBox();
+            this.tbxDuiAlumnos = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbxDuiAlumnos);
+            this.groupBox1.Controls.Add(this.tbxTelefonoAlumnos);
+            this.groupBox1.Controls.Add(this.tbxDireccionAlumnos);
+            this.groupBox1.Controls.Add(this.tbxNombreAlumnos);
+            this.groupBox1.Controls.Add(this.tbxCodigoAlumnos);
+            this.groupBox1.Controls.Add(this.LblDuiDelAlumno);
+            this.groupBox1.Controls.Add(this.LblTelefonoDelAlumno);
+            this.groupBox1.Controls.Add(this.LblDireccionDelAlumno);
+            this.groupBox1.Controls.Add(this.LblNombreDelAlumno);
+            this.groupBox1.Controls.Add(this.LblCodigoAlumno);
+            this.groupBox1.Location = new System.Drawing.Point(45, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(772, 298);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Formulario Alumnos ";
+            // 
+            // LblCodigoAlumno
+            // 
+            this.LblCodigoAlumno.AutoSize = true;
+            this.LblCodigoAlumno.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCodigoAlumno.Location = new System.Drawing.Point(22, 40);
+            this.LblCodigoAlumno.Name = "LblCodigoAlumno";
+            this.LblCodigoAlumno.Size = new System.Drawing.Size(62, 18);
+            this.LblCodigoAlumno.TabIndex = 0;
+            this.LblCodigoAlumno.Text = "Codigo:";
+            // 
+            // LblNombreDelAlumno
+            // 
+            this.LblNombreDelAlumno.AutoSize = true;
+            this.LblNombreDelAlumno.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNombreDelAlumno.Location = new System.Drawing.Point(22, 73);
+            this.LblNombreDelAlumno.Name = "LblNombreDelAlumno";
+            this.LblNombreDelAlumno.Size = new System.Drawing.Size(69, 18);
+            this.LblNombreDelAlumno.TabIndex = 1;
+            this.LblNombreDelAlumno.Text = "Nombre:";
+            // 
+            // LblDireccionDelAlumno
+            // 
+            this.LblDireccionDelAlumno.AutoSize = true;
+            this.LblDireccionDelAlumno.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDireccionDelAlumno.Location = new System.Drawing.Point(22, 103);
+            this.LblDireccionDelAlumno.Name = "LblDireccionDelAlumno";
+            this.LblDireccionDelAlumno.Size = new System.Drawing.Size(84, 18);
+            this.LblDireccionDelAlumno.TabIndex = 2;
+            this.LblDireccionDelAlumno.Text = "Direccion:";
+            // 
+            // LblTelefonoDelAlumno
+            // 
+            this.LblTelefonoDelAlumno.AutoSize = true;
+            this.LblTelefonoDelAlumno.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTelefonoDelAlumno.Location = new System.Drawing.Point(22, 137);
+            this.LblTelefonoDelAlumno.Name = "LblTelefonoDelAlumno";
+            this.LblTelefonoDelAlumno.Size = new System.Drawing.Size(77, 18);
+            this.LblTelefonoDelAlumno.TabIndex = 3;
+            this.LblTelefonoDelAlumno.Text = "Telefono:";
+            // 
+            // LblDuiDelAlumno
+            // 
+            this.LblDuiDelAlumno.AutoSize = true;
+            this.LblDuiDelAlumno.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDuiDelAlumno.Location = new System.Drawing.Point(22, 163);
+            this.LblDuiDelAlumno.Name = "LblDuiDelAlumno";
+            this.LblDuiDelAlumno.Size = new System.Drawing.Size(41, 18);
+            this.LblDuiDelAlumno.TabIndex = 4;
+            this.LblDuiDelAlumno.Text = "Dui:";
+            // 
+            // tbxCodigoAlumnos
+            // 
+            this.tbxCodigoAlumnos.Location = new System.Drawing.Point(137, 36);
+            this.tbxCodigoAlumnos.Name = "tbxCodigoAlumnos";
+            this.tbxCodigoAlumnos.Size = new System.Drawing.Size(199, 20);
+            this.tbxCodigoAlumnos.TabIndex = 6;
+            // 
+            // tbxNombreAlumnos
+            // 
+            this.tbxNombreAlumnos.Location = new System.Drawing.Point(137, 69);
+            this.tbxNombreAlumnos.Name = "tbxNombreAlumnos";
+            this.tbxNombreAlumnos.Size = new System.Drawing.Size(199, 20);
+            this.tbxNombreAlumnos.TabIndex = 7;
+            // 
+            // tbxDireccionAlumnos
+            // 
+            this.tbxDireccionAlumnos.Location = new System.Drawing.Point(137, 99);
+            this.tbxDireccionAlumnos.Name = "tbxDireccionAlumnos";
+            this.tbxDireccionAlumnos.Size = new System.Drawing.Size(199, 20);
+            this.tbxDireccionAlumnos.TabIndex = 8;
+            // 
+            // tbxTelefonoAlumnos
+            // 
+            this.tbxTelefonoAlumnos.Location = new System.Drawing.Point(137, 133);
+            this.tbxTelefonoAlumnos.Name = "tbxTelefonoAlumnos";
+            this.tbxTelefonoAlumnos.Size = new System.Drawing.Size(199, 20);
+            this.tbxTelefonoAlumnos.TabIndex = 9;
+            // 
+            // tbxDuiAlumnos
+            // 
+            this.tbxDuiAlumnos.Location = new System.Drawing.Point(137, 159);
+            this.tbxDuiAlumnos.Name = "tbxDuiAlumnos";
+            this.tbxDuiAlumnos.Size = new System.Drawing.Size(199, 20);
+            this.tbxDuiAlumnos.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "|<";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(45, 369);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(336, 104);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Navegacion ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Location = new System.Drawing.Point(455, 369);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(391, 104);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Navegacion";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(68, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(190, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(261, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = ">|";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(0, 38);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Nuevo";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(105, 38);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Modificar";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(316, 38);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "Buscar";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(204, 38);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "Eliminar";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 639);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbxDuiAlumnos;
+        private System.Windows.Forms.TextBox tbxTelefonoAlumnos;
+        private System.Windows.Forms.TextBox tbxDireccionAlumnos;
+        private System.Windows.Forms.TextBox tbxNombreAlumnos;
+        private System.Windows.Forms.TextBox tbxCodigoAlumnos;
+        private System.Windows.Forms.Label LblDuiDelAlumno;
+        private System.Windows.Forms.Label LblTelefonoDelAlumno;
+        private System.Windows.Forms.Label LblDireccionDelAlumno;
+        private System.Windows.Forms.Label LblNombreDelAlumno;
+        private System.Windows.Forms.Label LblCodigoAlumno;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
